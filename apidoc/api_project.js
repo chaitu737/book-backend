59b7d006f9c69bc58fc04c05451b8e00eb199f18
unknown <sahal737@gmail.com>
2019-10-11 17:44:48 +0530

    last-commit

---
+++ apidoc/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Book Backend",
+  "version": "0.0.1",
+  "description": "Documentation for the bookshell",
+  "title": "Book APIs",
+  "url": "http://localhost:3000",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-10-11T12:14:14.924Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
